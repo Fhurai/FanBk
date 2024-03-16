3beb966541b45c0b9e26fb8e7f91ca4af7b94a5a
Fhurai <kulu57@gmail.com>
2024-03-16 18:21:32 +0100

    Ajout UrlComponent pour crypter les actions & objets manipulés lors des appels ajax.
    Factorisation du code des controllers dans le parent AppController.
    Juste le code spécifique à chaque controller reste dans ceux ci.
    ObjectControllerInterface reçoit la méthode importFormOptions() pour les données à récupérer pour les formulaires.
    FanfictionsController & SeriesController sont aussi factorisés.
    Correction sur les formulaires pour échapper les caractères spéciaux dans les noms et descriptions d'entités & noms de variables suites aux factorisations.

--- templates/Series/edit.php
+++ templates/Series/edit.php
@@ -1,8 +1,8 @@
 <div class="row">
-    <?= $this->element("sidemenu/series", ["series" => $series, "action" => "edit"]) ?>
+    <?= $this->element("sidemenu/series", ["series" => $serie, "action" => "edit"]) ?>
     <div class="column-responsive column-80">
         <div class="series form content">
-            <?= $this->element("form/series", ["series" => $series, "action" => "edit"]) ?>
+            <?= $this->element("form/series", ["series" => $serie, "action" => "edit"]) ?>
         </div>
     </div>
 </div>
