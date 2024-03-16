3105ba17666033b4df1be497e169b0edf4eed8be
Fhurai <kulu57@gmail.com>
2024-03-16 16:46:33 +0100

    Series & Fanfictions retirées de la page d'accueil (seront remplacés par stats).

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -2,25 +2,8 @@
 
 /**
  * @var \App\View\AppView $this
- * @var iterable<\App\Model\Entity\Fanfiction> $fanfictions
- * @var array $parametres
  */
 ?>
 <div class="pages index content">
     <h1>Welcome on Fanfictions Bookmarks</h1>
-    <p>To add more stories in your horizon, here some unnoted stories.</p>
-    <hr />
-    <div>
-        <h4>Fanfictions</h4>
-        <?php foreach ($fanfictions as $fanfiction) : ?>
-            <?= $this->element("descriptif/fanfiction", ["fanfiction" => $fanfiction, "parametres" => $parametres, "edit" => false]) ?>
-        <?php endforeach; ?>
-    </div>
-    <hr />
-    <div>
-        <h4>Series</h4>
-        <?php foreach ($series as $serie) : ?>
-            <?= $this->element("descriptif/series", ["series" => $serie, "parametres" => $parametres, "edit" => false]) ?>
-        <?php endforeach; ?>
-    </div>
 </div>
